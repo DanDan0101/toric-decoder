c748145808ded8e68b84b8d0e7d95b0db73bc5b3
[Daniel Sun] <dansun@stanford.edu>
2024-08-20 15:04:58 -0700

    Automatic chunking for main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ t0 = time()
 import sys
 sys.path.insert(0, 'toric-decoder')
 
-# import numpy as np
+import numpy as np
 from toric import State, decoder_2D, mwpm, pcm, logical_error
 from pymatching import Matching
 
@@ -18,9 +18,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-n', type = int, default = 0)
 args = parser.parse_args()
 n = args.n # TODO
+debug = (n == -1)
 
-N = 7500
 L = 100
+
+N = int(200000000/L**2) # Assuming 16 GB of GPU memory
+if debug:
+    N = int(75000000/L**2) # Assuming 6 GB of GPU memory
+
+R = int(50*(L/100)**2) # Repetitions
+if R < 1 or debug:
+    R = 1
+
 p_error = 0.005
 η = 0.1
 c = 16
@@ -32,10 +41,13 @@ state = State(N, L)
 
 decoder_2D(state, T, c, η, p_error)
 
-x_correction, y_correction = mwpm(matching, state.q)
-fail = logical_error(x_correction ^ state.x_error.get(), y_correction ^ state.y_error.get())
+fails = np.empty(R)
 
-fail_rate = fail.mean() # Just a single float
+for i in range(R):
+    x_correction, y_correction = mwpm(matching, state.q)
+    fails[i] = logical_error(x_correction ^ state.x_error.get(), y_correction ^ state.y_error.get()).mean()
+
+fail_rate = fails.mean() # Just a single float
 
 print(f"Fail rate: {fail_rate}")
 
